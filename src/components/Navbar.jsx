3b9042c7b4f4c43cbea771c95b9e1eac16a37dd4
syed umer <umershakir987@gmail.com>
2026-08-11 00:51:33 +0500

    starting project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,9 @@ function Navbar() {
     <div>
     <nav>
       <div className='w-[100%] h-[50px] flex justify-center items-center'>
+       <div className='md:hidden lg:hidden w-[100%] flex justify-start items-center '><h1 className='font-extrabold text-[20px] whitespace-nowrap'>S_M_U</h1>  </div>
 
-<div className='flex w-[30%] justify-center items-center'>
+<div className='md:flex w-[30%] justify-center items-center hidden'>
         <ul className='flex gap-7 font-bold'>
         <li>Home</li>
         <li>About</li>
@@ -14,8 +15,8 @@ function Navbar() {
         </ul>
         </div>
       
-       <div className='w-[40%] flex justify-center items-start '><h1 className='font-extrabold text-5xl'>SYED MUHAMMAD UMER</h1> </div>
-     <div className='flex w-[30%] justify-center items-center'>
+       <div className='w-[40%] md:flex justify-center items-start hidden '><h1 className='font-extrabold text-5xl whitespace-nowrap'>SYED MUHAMMAD UMER</h1> </div>
+     <div className='md:flex w-[30%] justify-center items-center hidden'>
         <ul className='flex gap-7 font-bold'>
         <li>Projects</li>
         <li>Experience</li>
