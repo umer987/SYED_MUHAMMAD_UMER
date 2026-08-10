900ed560d1f3d535be71972a670d7518f3aeb0fa
syed umer <umershakir987@gmail.com>
2026-08-10 11:42:24 +0500

    initial settings

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Routes , Route , BrowserRouter} from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div>
+        <BrowserRouter>
+        <Routes>  {/* ← Changed: Router → Routes */}
+          <Route path='/' element={<Home />} />
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
