13392f1d977db7e7a1a18c60f82bd573d76afe44
syed umer <umershakir987@gmail.com>
2026-08-11 00:54:28 +0500

    starting project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
     <nav>
       <div className='w-[100%] h-[50px] flex justify-center items-center'>
-       <div className='md:hidden lg:hidden w-[100%] flex justify-start items-center '><h1 className='font-extrabold text-[20px] whitespace-nowrap'>S_M_U</h1>  </div>
+       <div className='md:hidden lg:hidden w-[100%] flex justify-start items-center '><h1 className='font-extrabold text-[20px] whitespace-nowrap'>S_M_U</h1> <img src="" alt="" srcset="" /> </div>
 
 <div className='md:flex w-[30%] justify-center items-center hidden'>
         <ul className='flex gap-7 font-bold'>
