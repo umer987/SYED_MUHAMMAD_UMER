1f0aa6539a57b23938b12ca403097ca8ce7cc9ff
syed umer <umershakir987@gmail.com>
2026-08-12 00:30:00 +0500

    navbar added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     <div>
-    <div className='w-[100%] flex justify-center items-center '> <h1 className='hero-huge hero '>SYED MUHAMMAD UMER</h1></div>
+    <div className='w-[100%] flex justify-center items-center '> <h1 className='md:hero-huge hero-huges '>SYED MUHAMMAD UMER</h1></div>
     
     
     </div>
