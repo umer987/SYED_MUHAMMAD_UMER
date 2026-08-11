dab2eb1aed9f54a1f727df0dc5ae3af68e6ddc44
syed umer <umershakir987@gmail.com>
2026-08-12 00:59:45 +0500

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
       
       <div className='flex-1 h-[1.5px] md:h-[2px] rounded-3xl bg-[var(--text-bg)] mx-2 '></div>
       
-      {/* Right Text & Red Dot */}
+     
       <div className='font-bebas font-thin flex items-center gap-1 md:gap-2 text-[var(--text-bg)] text-[0.5rem] tracking-[0.5px] md:text-base md:tracking-[3px] uppercase'>
         <h3>AVAILABLE FOR FREELANCE</h3>
         <div className='w-[6px] h-[6px] md:w-[10px] md:h-[10px] bg-[var(--bg-red)] rounded-full flex-shrink-0'></div>
