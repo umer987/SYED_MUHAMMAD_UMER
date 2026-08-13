a9127124d0e2265842be1df3cd11e280e39ac850
syed umer <umershakir987@gmail.com>
2026-08-14 00:19:26 +0500

    finishing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
   return (
     <div>
     <div className='w-full flex justify-center items-center '>
-      <h1 className='font-bebas inline-block uppercase origin-bottom scale-y-[1.3] text-[#E7CEB0] text-[3rem] leading-[0.4] font-bold mt-[2rem] md:text-[10rem] md:leading-[0.8] md:font-[900] md:tracking-[10px] md:mt-[5rem]'>
+      <h1 className='font-bebas inline-block uppercase origin-bottom scale-y-[1.3] text-[#E7CEB0] text-[2.8rem] leading-[0.4] font-bold mt-[2rem] md:text-[10rem] md:leading-[0.8] md:font-[900] md:tracking-[10px] md:mt-[5rem]'>
         SYED MUHAMMAD UMER
       </h1>
     </div>
@@ -13,10 +13,10 @@ function Hero() {
 
   {/* 1 - Headings */}
   <div className="order-2 md:order-1 "  >
-    <h1 className='font-bebas text-[#E7CEB0] text-2xl md:text-4xl'>FULLSTACK ENGINEER / AI-ML ENGINEER</h1>
-    <h3 className='font-bebasl font-extralight text-[#E7CEB0] text-[1.2rem] md:text-[1.5] '>Web Development</h3>
-    <h3 className='font-bebasl font-extralight text-[#E7CEB0] text-[1.2rem] md:text-[1.5] '>Intellegent Systems</h3>
-    <h3 className='font-bebasl font-extralight text-[#E7CEB0] text-[1.2rem] md:text-[1.5] '>ML Models</h3>
+    <h1 className='font-bebas text-[#E7CEB0] text-2xl md:text-4xl'>FULL-STACK ENGINEER / AI-ML ENGINEER</h1>
+    <h3 className='font-bebasl font-extralight text-[#E7CEB0] text-[1.2rem] md:text-[1.5rem] '>Web Development</h3>
+    <h3 className='font-bebasl font-extralight text-[#E7CEB0] text-[1.2rem] md:text-[1.5rem] '>Intellegent Systems</h3>
+    <h3 className='font-bebasl font-extralight text-[#E7CEB0] text-[1.2rem] md:text-[1.5rem] '>ML Models</h3>
     <div className='w-[30px] md:w-[40px] h-[5px] bg-[#79231C] rounded-2xl mt-3.5'></div>
     <div className='flex mt-6 gap-2'>
       <img className='w-[30px] h-[20px]  md:w-[80px] md:h-[50px]'  src="/comma.png" alt="" srcset="" />
@@ -29,17 +29,36 @@ function Hero() {
 
   {/* 2 - Image */}
   <div className="order-1 md:order-2">
-    <img src="/umer.png" alt="Umer" />
+    <img className='w-[350px] ' src="/umer.png" alt="Umer" />
   </div>
 
   {/* 3 - Heading + paragraph */}
   <div className="order-3 md:order-3">
-    <h2>About me</h2>
-    <p>
-      Software Engineer and MERN Stack Developer...
-    </p>
-  </div>
+    <h2 className='font-bebas text-[var(--text-bg)] mt-1 text-4xl '>About me</h2>
+    <h2 className='text-[var(--bg-red)] font-bebas mt-1 text-3xl'>AI/ML & Full-Stack Web Developer</h2>
 
+    <p className='font-bebas text-[var(--text-bg)] mt-1 text-[12px] leading-relaxed tracking-wide md:text-[20px] '>
+      I’m a Full-Stack Web Developer & AI/ML Engineer focused on building<br/> modern, scalable web applications and intelligent solutions. I combine <br/>strong development skills with AI/ML to create smart, efficient, and<br/> impactful digital experiences.
+    </p>
+    <div className='w-[280px] h-[200px] border-t-3 mt-3.5 flex border-[#79231C] gap-12'>
+      <div className='text-left text-[var(--c-text)] mt-3.5'>
+        <img src="/experience.png"  className='w-[50px] h-[50px]' alt="" srcset="" />
+        <h1 className='font-bebas text-2xl leading-relaxed tracking-wide'>4+</h1>
+        <h3 className='font-bebas'>YEARS <br/> EXPERIENCE</h3>
+      </div>
+      <div className='text-left text-[var(--c-text)] mt-3.5' >
+        <img src="/projects.png" className='w-[50px] h-[50px]'  alt="" srcset="" />
+        <h1 className='font-bebas text-2xl leading-relaxed tracking-wide'>23+</h1>
+        <h3 className='font-bebas'>projects <br/>complete</h3>
+      </div>
+      <div className='text-left text-[var(--c-text)] mt-3.5'>
+        <img src="/clients.png" className='w-[50px] h-[50px]'  alt="" srcset="" />
+        <h1 className='font-bebas text-2xl leading-relaxed tracking-wide'>12+</h1>
+        <h3 className='font-bebas'>happy <br/>clients</h3>
+      </div>
+    </div>
+
+  </div>
 </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div className='w-full flex justify-between items-center'>
       
       <div>
-        <h3 className='font-bebas font-bold text-[var(--text-bg)] text-[0.5rem] tracking-[0.5px] md:text-base md:tracking-[3px] uppercase'>
+        <h3 className='font-bebas  text-[var(--text-bg)] text-[0.7rem] tracking-[0.5px] md:text-base md:tracking-[3px] uppercase'>
           FULLSTACK AND AI/ML ENGINEER
         </h3>
       </div>
@@ -14,9 +14,9 @@ function Navbar() {
       <div className='flex-1 h-[1.5px] md:h-[2px] rounded-3xl bg-[var(--text-bg)] mx-2 '></div>
       
      
-      <div className='font-bebas font-thin flex items-center gap-1 md:gap-2 text-[var(--text-bg)] text-[0.5rem] tracking-[0.5px] md:text-base md:tracking-[3px] uppercase'>
+      <div className='font-bebas  flex items-center gap-1.5 md:gap-2 text-[var(--text-bg)] text-[0.7rem] tracking-[0.5px] md:text-base md:tracking-[3px] uppercase'>
         <h3>AVAILABLE FOR FREELANCE</h3>
-        <div className='w-[6px] h-[6px] md:w-[10px] md:h-[10px] bg-[var(--bg-red)] rounded-full flex-shrink-0'></div>
+        <div className='w-[8px] h-[8px] md:w-[10px] md:h-[10px] bg-[var(--bg-red)] rounded-full flex-shrink-0'></div>
       </div>
       
     </div>
