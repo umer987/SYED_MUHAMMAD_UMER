c5e3856a95fc65d8067da6e7be2cb48594142810
syed umer <umershakir987@gmail.com>
2026-08-16 01:26:47 +0500

    complete section 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,12 +36,12 @@ function Hero() {
   {/* 3 - Heading + paragraph */}
   <div className="order-3 md:order-3 md:mt-8 flex-col  justify-center items-center">
     <h2 className='font-bebas text-[var(--text-bg)] mt-1 text-4xl '>About me</h2>
-    <h2 className='text-[var(--bg-red)] font-bebas mt-1 text-3xl'>AI/ML & Full-Stack Web Developer</h2>
+    <h2 className='text-[var(--bg-red)] font-bebas mt-1 md:text-3xl'>AI/ML & Full-Stack Web Developer</h2>
 
     <p className='font-bebas text-[var(--text-bg)] mt-1 text-[12px] leading-relaxed tracking-wide md:text-[20px] '>
       I’m a Full-Stack Web Developer & AI/ML Engineer focused on<br/> building modern, scalable web applications and intelligent<br/> solutions. I combine strong development skills with AI/ML<br/> to create smart, efficient, and impactful digital experiences.
     </p>
-    <div className='w-[280px] h-[200px] border-t-3 mt-3.5 flex border-[#79231C] gap-12'>
+    <div className='w-[280px] h-[200px] border-t-3 mt-3.5 flex border-[#79231C] gap-16 md:gap-12'>
       <div className='text-left text-[var(--c-text)] mt-3.5'>
         <img src="/experience.png"  className='w-[50px] h-[50px]' alt="" srcset="" />
         <h1 className='font-bebas text-2xl leading-relaxed tracking-wide'>4+</h1>
