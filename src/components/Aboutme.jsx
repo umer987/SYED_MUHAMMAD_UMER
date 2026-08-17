4199be6fcacb8fd47c8e16c49565aab9a0b33705
syed umer <umershakir987@gmail.com>
2026-08-18 00:12:23 +0500

    adding sec 4

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Aboutme() {
   return (
-    <div className='flex '>
-      <div className='w- h-300px bg-amber-300'></div>
-      <div className='w- h-300px bg-amber-300'></div>
+    <div className='flex w-full h-300px '>
+      <div className='w-[50%] h-300px bg-amber-300'></div>
+      <div className='w-[50%] h-300px bg-amber-300'></div>
     </div>
   )
 }
