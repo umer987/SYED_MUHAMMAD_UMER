ebe0ae9d39e773aad1f5107025989898abceb9ee
syed umer <umershakir987@gmail.com>
2026-08-23 00:20:15 +0500

    add project mofdule

--- backend/src/routes/project.route.js
+++ backend/src/routes/project.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const projectcontroller = require('../controllers/project.middleware')
+const projectcontroller = require('../controllers/project.controller')
 
 router.post('/add-project' , projectcontroller.projectcontroller)
 
