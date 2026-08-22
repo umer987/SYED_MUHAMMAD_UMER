a8a0535ecf9df970e45e5d26aa29ccb0296784da
syed umer <umershakir987@gmail.com>
2026-08-23 00:19:16 +0500

    add project mofdule

---
+++ backend/src/controllers/project.controller.js
@@ -0,0 +1,10 @@
+const projectmodel = require('../config/models/projects.model')
+
+async function projectcontroller(req,res) {
+     const {title ,category ,image ,discription} = req.body
+     console.log(title ,category ,image ,discription)
+}
+
+module.exports = {
+    projectcontroller
+};
