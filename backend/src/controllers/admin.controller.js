741d7bec3ba1d85959f20923a79e0222f029fd13
syed umer <umershakir987@gmail.com>
2026-08-24 00:32:31 +0500

    connecting database

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 async function admincontroller(req , res) {
     const {email , name , password } = req.body 
-\\    const hash = await bcrypt.hash(password,10)
+\   const hash = await bcrypt.hash(password,10)
     const admin = await adminmodel.create({
         email:email,
         name:name,
