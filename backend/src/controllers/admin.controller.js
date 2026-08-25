bcba6866c454981f85e3bae4e6aa7e80675fe5e1
syed umer <umershakir987@gmail.com>
2026-08-25 13:05:05 +0500

    adding auth

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -18,12 +18,13 @@ async function admincontroller(req , res) {
 
 async function logincontroller(req , res) {
     const {email , password } = req.body 
- const hash = await bcrypt.
-    const admin = await adminmodel.create({
+ const hash = await bcrypt.compare(password)
+ if(hash){
+    const admin = await adminmodel.findOne({
         email:email,
-        name:name,
-        password:hash
+        
     })
+    }
     const token = jwt.sign({id:admin._id},process.env.JWT)
     res.cookie("token", token)
      return res.status(200).json({
