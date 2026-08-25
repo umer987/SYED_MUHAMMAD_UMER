86bbea7806c1efe368d6d090f9bcd42d438fa04b
syed umer <umershakir987@gmail.com>
2026-08-26 00:58:11 +0500

    adding contact me module

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,5 +2,8 @@ const messagemodel = require('../config/models/message.model')
 
 async function messagecontroller(req,res) {
     const {email , name , projectbreif} = req.body
-    
+    const message = messagemodel.create({
+        email:email,
+        name:name
+    })
 }
