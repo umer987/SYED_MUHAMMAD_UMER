93b795f70da98120652b963b4162ed3b61a48bd3
syed umer <umershakir987@gmail.com>
2026-08-27 00:03:28 +0500

    adding admin panel

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 
 const AddProject = () => {
+  return (
+    <div className="flex items-center justify-center font-oswald w-full">
+      <div className="bg-[#201011] border border-[#79231C] p-8 rounded-lg w-full max-w-2xl shadow-lg mt-8 mb-8">
+        <h2 className="text-3xl text-white text-center mb-8 tracking-wide">UPLOAD NEW PROJECT</h2>
