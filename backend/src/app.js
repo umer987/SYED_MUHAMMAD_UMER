445001213b1ff3511035bebea125937ad9b0190b
syed umer <umershakir987@gmail.com>
2026-09-06 00:53:52 +0500

    deploying backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cookie =require('cookie-parser')
 const express  = require('express')
 const app = express()
 app.use(cors({
-  origin: ['http://localhost:5173'], // Must be the exact URL of your React frontend
+  origin: ['http://localhost:5173','https://syed-m-umer.vercel.app/'], // Must be the exact URL of your React frontend
   credentials: true,               // Required to allow cookies to pass through
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
